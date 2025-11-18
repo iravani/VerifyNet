b78e43775ef9ecb28f23fc9812cdf425bddff38f
iravani <59196609+iravani@users.noreply.github.com>
2025-11-18 14:09:19 +0330

    datastructure added

---
+++ src/com/ui/writers/iravani/talebi/crypto/primitives/data/structure/BigIntTupple.java
@@ -0,0 +1,11 @@
+package com.ui.writers.iravani.talebi.crypto.primitives.data.structure;
+
+import java.math.BigInteger;
+
+public class BigIntTupple {
+	BigInteger i1, i2;
+	
+	public BigIntTupple(BigInteger m, BigInteger n) {
+		i1 = m; i2 = n;
+	}
+}
